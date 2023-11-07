978c1a66e4977775971036e2e7db12f70e48f2ae
hmdmasudsarder <hmmasudsarder@gmail.com>
2023-11-07 23:10:10 +0600

    single products get

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
@@ -39,6 +39,12 @@ async function run() {
       const cursor = productCollection.find().sort({ orders: -1 }).limit(6)
       const result = await cursor.toArray();
       res.send(result);
+    });
+    app.get('/products/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await productCollection.findOne(query);
+      res.send(result)
     })
 
     // Send a ping to confirm a successful connection
